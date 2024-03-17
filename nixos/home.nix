b267c7693d1d7830df90e643252cb44cc13fe296
PaulUlanovskij <ulanovskijpasa@gmail.com>
2024-03-17 22:45:55 +0200

    fix package declaration

--- nixos/home.nix
+++ nixos/home.nix
@@ -120,16 +120,6 @@
 xdg.portal.enable = true;
 xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-sound.enable = true;
-security.rtkit.enable = true;
-services.pipewire = {
-  enable = true;
-  alsa.enable = true;
-  alsa.support32Bit = true;
-  pulse.enable = true;
-  jack.enable = true;
-};
-
 programs.git = {
 	enable = true;
     userName = "PaulUlanovskij";
